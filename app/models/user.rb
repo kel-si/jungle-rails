e0aec0860ac9d5e71c6b7acc7b2221a7bfe11416
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-12 14:38:34 -0600

    authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ActiveRecord::Base
 
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 3}
   
+  def self.authenticate_with_credentials email, password
+    user = User.find_by_email(email)
+    if user
+      if user.authenticate password
+      user
+      end
+    else
+      nil
+    end
+  end
 end
