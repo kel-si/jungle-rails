a458fd33242aec84dc437719de77430641c7aec3
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-11 22:17:37 -0600

    require email when creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, uniqueness: true
