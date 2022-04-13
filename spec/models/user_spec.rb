1646f29bf5a0d3734736cbf88beba1c0baf77f98
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-12 21:14:36 -0600

    single product page from home page test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
     end
 
-    it "should require a min. password length of 6" do
-      user = User.new(first_name: "Taylor", last_name: "Swift", email: "example@gmail.com", password: "123", password_confirmation: "123")
+    it "should require a min. password length of 3" do
+      user = User.new(first_name: "Taylor", last_name: "Swift", email: "example@gmail.com", password: "1", password_confirmation: "1")
       expect(user).to_not be_valid
     end
 
